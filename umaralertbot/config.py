629b63d94360a7cb701ec490a7ff5e2fbc0e13d7
Parow01 <umarfarukabdullahi2019@gmail.com>
2025-07-27 23:55:19 +0100

    Create config.py

---
+++ umaralertbot/config.py
@@ -0,0 +1,7 @@
+import os
+
+# Telegram Bot Token
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8092340392:AAHJN8d8mjQgHQeSAEyIYjEu0PesfQf0GH4")
+
+# Your Render webhook URL
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://parow01.onrender.com")
