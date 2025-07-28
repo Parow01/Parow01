75d52614a51f1e5c032ddf68cb4a6a17523feea8
Parow01 <umarfarukabdullahi2019@gmail.com>
2025-07-28 20:32:53 +0100

    Update keepalive.py

--- umaralertbot/keepalive.py
+++ umaralertbot/keepalive.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "UmarAlertBot is alive!"
+    return "Parownewbot is alive!"
 
 def keep_alive():
     app.run(host='0.0.0.0', port=8080)
