59d2db777ab6f5b2340cc11ca7c015c3f2ed3137
Parow01 <umarfarukabdullahi2019@gmail.com>
2025-07-28 08:40:38 +0100

    Update keepalive.py

--- umaralertbot/keepalive.py
+++ umaralertbot/keepalive.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ UmarAlertBot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    app = Flask(__name__)
-
-    @app.route("/")
-    def index():
-        return "UmarAlertBot is active!"
-
-    app.run(host="0.0.0.0", port=8080)
+    t = threading.Thread(target=run)
+    t.start()
