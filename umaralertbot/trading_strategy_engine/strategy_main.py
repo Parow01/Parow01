b28c2e7a857974ec0a38767145824d81b61e13bb
Parow01 <umarfarukabdullahi2019@gmail.com>
2025-07-30 19:42:56 +0100

    Update strategy_main.py

--- umaralertbot/trading_strategy_engine/strategy_main.py
+++ umaralertbot/trading_strategy_engine/strategy_main.py
@@ -1,9 +1,11 @@
-# strategy_main.py
+def start_trading_engine(scheduler):
+    from trading_strategy_engine.strategy_core import generate_trading_signal
+    from alert_manager.alert_handler import send_alert_to_telegram
 
-from trading_strategy_engine.strategy_core import generate_trading_signal
-from alert_manager.alert_handler import send_alert_to_telegram
+    def job():
+        signal = generate_trading_signal()
+        if signal:
+            send_alert_to_telegram(signal)
 
-def start_strategy_engine():
-    signal = generate_trading_signal()
-    if signal:
-        send_alert_to_telegram(signal)
+    scheduler.add_job(job, trigger='interval', minutes=5, id='trading_engine_job', replace_existing=True)
+
