211f6c799fc66d3411866244202fe820d8417ab4
Parow01 <umarfarukabdullahi2019@gmail.com>
2025-07-31 22:20:27 +0100

    Update smartlist_main.py

--- umaralertbot/whale_smartlist/smartlist_main.py
+++ umaralertbot/whale_smartlist/smartlist_main.py
@@ -2,14 +2,14 @@
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from whale_smartlist.smartlist_core import fetch_smart_whale_activity
-from alert_manager.router_main import send_alert_to_telegram
+from alert_manager.alert_handler import send_alert_to_telegram
 
-def start_smartlist_engine(scheduler: BackgroundScheduler):
-    scheduler.add_job(run_smartlist_engine, "interval", minutes=3, id="smartlist_engine", replace_existing=True)
-    print("[whale_smartlist] Smartlist engine scheduled every 3 minutes")
+def start_smartlist_monitor(scheduler: BackgroundScheduler):
+    def job():
+        alerts = fetch_smart_whale_activity()
+        for alert in alerts:
+            send_alert_to_telegram(alert)
 
-def run_smartlist_engine():
-    alerts = fetch_smart_whale_activity()
-    for alert in alerts:
-        send_alert_to_telegram(alert)
+    scheduler.add_job(job, trigger='interval', minutes=5, id='smartlist_monitor_job', replace_existing=True)
+
 
