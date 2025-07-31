ad4a28802b04e3c1cc39e7236053edd62c1b372e
Parow01 <umarfarukabdullahi2019@gmail.com>
2025-07-31 21:24:11 +0100

    Update alert_main.py

--- umaralertbot/alert_engine/alert_main.py
+++ umaralertbot/alert_engine/alert_main.py
@@ -1,6 +1,6 @@
 import time
 from alert_engine.alert_core import collect_all_alerts
-from message_router.router_main import send_alert_to_telegram
+from alert_manager.router_main import send_alert_to_telegram
 
 def run_alert_engine():
     print("[AlertEngine] Starting master alert loop...")
